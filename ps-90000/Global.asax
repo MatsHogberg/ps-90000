12b99bc009f6cb57c2ba1f6d40f1da5254a63028
Mats Högberg <mats.hogberg@cybercom.com>
2017-12-07 09:20:48 +0100

    Add project files.

---
+++ ps-90000/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ps_90000.MvcApplication" Language="C#" %>
---
+++ ps-90000/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ps_90000.Startup))]
+namespace ps_90000
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
